931f542db935f571e9e13aaf3a3dc35fe2b5c2b1
Himel Das <himelhub@gmail.com>
2020-08-14 11:24:13 +0600

    refactor

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,10 +1,27 @@
-# from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render
+
+posts = [
+    {
+        'author': 'John Smith',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'September 20, 2020'
+    },
+    {
+        'author': 'Eugene T. Bradford',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'September 22, 2020'
+    }
+]
 
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
