c702a94304e2aa3f4aa620eb2af2a3e8ef5b25ae
Himel Das <himelhub@gmail.com>
2020-08-14 14:01:58 +0600

    refactor

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+# User is the bult-in model provided by django
+# User-Post has one-to-many relationship.
+# One user can have multiple posts.
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
